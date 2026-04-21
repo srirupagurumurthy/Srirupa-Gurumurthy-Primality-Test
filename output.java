121fd72a2c5fb9a97c0e7f4c1b56a33173085968
srirupagurumurthy <814724104148@trp.srmtrichy.edu.in>
2026-04-21 20:36:10 +0530

    Create output.java

---
+++ output.java
@@ -0,0 +1,9 @@
+Enter a number to test: 997
+
+Results for 997:
+
+Trial Division: true | Time: 0.0082 ms
+Miller-Rabin: true | Time: 0.1543 ms
+Fermat Test: true | Time: 0.1321 ms
+
+Run benchmark? y/n:
